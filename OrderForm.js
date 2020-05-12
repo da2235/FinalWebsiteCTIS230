36d05dfc5d2cad488f11a9ce55327471c0883793
da2235 <64100980+da2235@users.noreply.github.com>
2020-05-11 22:08:55 -0400

    dev

---
+++ OrderForm.js
@@ -0,0 +1,18 @@
+myFirstName = document.getElementById("first name");
+myLastName = document.getElementById("last name");
+myEmail = document.getElementById("email");
+myPhoneNumber = document.getElementById("phone number");
+myBakeryItem = document.getElementById("bakery item");
+myQuantity = document.getElementById("quantity");
+mySpecialInstructions = document.getElementById("special instructions");
+myCreditCardNumber = document.getElementById("credit card number");
+mySecurityCode = document.getElementById("security code");
+myExpirationDate = document.getElementById("expiration date");
+myAdditional = document.getElementById("additional information");
+
+function formfunction () {
+    console.log(myFirstName.value);
+    document.write("<h1>");
+    document.write("myFirstName.value");
+    document.write("</h1>");
+}
