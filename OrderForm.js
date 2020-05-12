5e0f2b755388dec8514932ef9fb38eb1952c73f1
da2235 <64100980+da2235@users.noreply.github.com>
2020-05-12 09:16:21 -0400

    -

--- OrderForm.js
+++ OrderForm.js
@@ -13,56 +13,59 @@ myAdditional = document.getElementById("additional information");
 function formfunction () {
     console.log(myFirstName.value);
     document.write("<h1>");
-    document.write(myFirstName.value);
+    document.write("Your order form summary:");
     document.write("</h1>");
+    document.write("<p>");
+    document.write(myFirstName.value);
+    document.write("</p>");
 
     console.log(myLastName.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(myLastName.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(myEmail.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(myEmail.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(myPhoneNumber.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(myPhoneNumber.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(myBakeryItem.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(myBakeryItem.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(myQuantity.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(myQuantity.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(mySpecialInstructions.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(mySpecialInstructions.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(myCreditCardNumber.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(myCreditCardNumber.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(mySecurityCode.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(mySecurityCode.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(myExpirationDate.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(myExpirationDate.value);
-    document.write("</h1>");
+    document.write("</p>");
 
     console.log(myAdditional.value);
-    document.write("<h1>");
+    document.write("<p>");
     document.write(myAdditional.value);
-    document.write("</h1>");
+    document.write("</p>");
 }
